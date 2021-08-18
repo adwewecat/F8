e8249dde28531d09e3bcd9006af94a64d85e505d
adwewecat <89115538+adwewecat@users.noreply.github.com>
2021-08-18 21:38:09 +0700

    'luu1'

---
+++ z1dowload-code-tren-Github.bat
@@ -0,0 +1,2 @@
+CD /d d:\lap_trinh_vip\4.F8.EDU.VN\project_f8_p1
+git pull origin master
---
+++ zupload-project-len-Github.bat
@@ -0,0 +1,13 @@
+CD /d d:\lap_trinh_vip\4.F8.EDU.VN\project_f8_p1
+
+git status
+
+git add -A
+
+GIT commit -m 'luu1'
+
+GIT push origin master
+
+Echo Chao Tam Biet
+
+PAUSE
